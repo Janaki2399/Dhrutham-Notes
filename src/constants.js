7723ce484d9c1eb8eaa4e44ce34f203fae13bbac
Janaki <janakishreeja23@gmail.com>
2021-07-10 16:20:59 +0530

    refactor

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-export const API_URL = "http://localhost:5000";
+export const API_URL = "https://dhrutham-notes-backend.herokuapp.com";
 export const API_STATUS = {
   IDLE: "idle",
   LOADING: "loading",
